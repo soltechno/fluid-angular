1de83084a137300f19e65516e59be85dc66002a7
dmao <didier.mahaux@gmail.com>
2024-09-13 12:03:54 +0000

    Gameplay component

---
+++ src/app/gameplay/gameplay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gameplay',
+  standalone: true,
+  imports: [],
+  templateUrl: './gameplay.component.html',
+  styleUrl: './gameplay.component.css'
+})
+export class GameplayComponent {
+
+}
