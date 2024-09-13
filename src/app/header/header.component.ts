9ef7bcd04817bfa29a423feebad260a5748082c6
dmao <didier.mahaux@gmail.com>
2024-09-13 12:03:15 +0000

    Header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+	currentTime: string = '';
+	private intervalId: any;
+
+	ngOnInit() {
+		this.updateTime();
+		this.intervalId = setInterval(() => this.updateTime(), 60000); // Update time every minute
+	}
+
+	ngOnDestroy() {
+		if (this.intervalId) {
+			clearInterval(this.intervalId);
+		}
+	}
+
+	updateTime() {
+		const now = new Date();
+		this.currentTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+	}
 
 }
