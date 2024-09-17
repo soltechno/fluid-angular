0939346a08f8633bb10db98c22546594b13be5c6
dmao <didier.mahaux@gmail.com>
2024-09-17 10:44:50 +0000

    Add title to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './features/home/home.component';
 import { GameplayComponent } from './features/gameplay/gameplay.component';
 
 export const routes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: 'gameplay', component: GameplayComponent },
+	{ path: '',title: 'Fluid demo', component: HomeComponent },
+	{ path: 'gameplay', title: 'Gameplay | Fuid demo', component: GameplayComponent },
 ];
