6820cace52ebd16a9a546e771e361b4a5865fb58
dmao <didier.mahaux@gmail.com>
2024-09-17 10:46:51 +0000

    Update footer in gameplay

--- src/app/features/gameplay/footer-mobile/footer-mobile.component.ts
+++ src/app/features/gameplay/footer-mobile/footer-mobile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
 	selector: 'app-footer-mobile',
 	standalone: true,
-	imports: [],
+	imports: [RouterOutlet, RouterLink, RouterLinkActive],
 	templateUrl: './footer-mobile.component.html',
 	styleUrl: './footer-mobile.component.css'
 })
