c1aa06056b3ef2b88ce469bd5eb8ac85d2b9db05
adam <adam.rojek@gmail.com>
2025-02-11 15:53:26 +0530

    Fixing routing to avoid full page reload

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, SharedModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
