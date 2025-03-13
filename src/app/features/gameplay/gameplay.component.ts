95bc9e001e0e87623b202cd08efb9ac2b942c11c
dmao <didier.mahaux@gmail.com>
2025-03-13 10:57:51 +0000

    Add inline quick deposit to game play

--- src/app/features/gameplay/gameplay.component.ts
+++ src/app/features/gameplay/gameplay.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, CUSTOM_ELEMENTS_SCHEMA, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game/game.component';
 import { FooterMobileComponent } from './footer-mobile/footer-mobile.component';
@@ -16,7 +16,8 @@ import { ToolsComponent } from './tools/tools.component';
 		FooterDesktopComponent,
 	],
 	templateUrl: './gameplay.component.html',
-	styleUrl: './gameplay.component.css'
+	styleUrl: './gameplay.component.css',
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
 export class GameplayComponent {
@@ -25,4 +26,14 @@ export class GameplayComponent {
 	onQuickDepositClick() {
 		this.quickDepositClicked.emit();
 	}
+
+	transaction = 'deposit';
+	bonuses: any = null;
+	loggedIn = true;
+	operatorId: string | null = null;
+	userId: string | null = null;
+	sessionId: string | null = null;
+	depositLimit: number | string = "";
+	balance: number = 1000;
+	withdrawableBalance: number = 1000;
 }
